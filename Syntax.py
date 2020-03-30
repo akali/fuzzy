99b440e798338680bd7434b12898f09270db387a
Aisultan Kali <aisultan.kali@gmail.com>
2020-03-31 05:41:41 +0600

    [WIP] feat(syntax): implement syntax parse

--- Syntax.py
+++ Syntax.py
@@ -1,7 +1,9 @@
 import json
+from copy import deepcopy
 
 import settings
 from Hedge import dict_hedges
+from MembershipFunction import MembershipFunction
 
 
 def get_synonyms(word):
@@ -33,7 +35,7 @@ def get_hedges():
     return dict_hedges().keys()
 
 
-def get_hedges_synonyms(limit):
+def get_hedges_synonyms(limit=100):
     """
 
     :param limit: similarity limit
@@ -55,13 +57,20 @@ class SyntaxException(BaseException):
     pass
 
 
-def to_sql(fuzzy_query, fields, limit=100):
+def to_sql(fuzzy_query: str, fields, limit=100, alpha_cut=0.5):
     """
     TODO: Implement query parse
-    Converts fuzzy query to sql query
-    :param fields: dict of querying numerical fields: {name, membership_function}
-    :param limit: similarity limit for synonyms
+    Converts fuzzy where clause query to sql where clause query.
+    Fuzzy expression structure:
+    [hedge] [hedge] [hedge] ... [hedge] [membership function] [field] [connector]
+    [hedge] [hedge] [hedge] ... [hedge] [membership function] [field] [connector]
+    [hedge] [hedge] [hedge] ... [hedge] [membership function] [field] [connector] ...
+    [hedge] [hedge] [hedge] ... [hedge] [membership function] [field]
+
     :param fuzzy_query: fuzzy query
+    :param fields: dict of querying numerical fields: {field_name, {membership_function_name: membership_function}}
+    :param limit: similarity limit for synonyms
+    :param alpha_cut:
     :return: sql query
     :raises: SyntaxException: syntax error
     """
@@ -69,4 +78,55 @@ def to_sql(fuzzy_query, fields, limit=100):
     if fuzzy_query == "":
         raise SyntaxException("Empty query")
 
-    return ""
+    tokens = fuzzy_query.split(' ')
+    crisp_query = ""
+
+    hedges_synonyms = get_hedges_synonyms(limit)
+    hedges = dict_hedges()
+
+    connectors = ["and", "or"]
+    default_mfs = {
+        "low": {},
+        "middle": {},
+        "high": {},
+    }
+
+    expression = []
+
+    for token in tokens:
+        if token in connectors:
+            if len(expression) < 2:
+                raise SyntaxException(f"Empty or incorrect expression {expression}")
+            original_expression = expression
+            expression.reverse()
+            if expression[0] not in fields.keys():
+                raise SyntaxException(f"Unknown field {expression[0]} in expression {original_expression}")
+            field = expression.pop(0)
+
+            mf_name = expression[0]
+
+            if mf_name not in default_mfs.keys() and mf_name not in fields[field].keys():
+                raise SyntaxException(f"Unknown membership function {mf_name} in expression {original_expression}")
+
+            if mf_name in fields[field].keys():
+                mf: MembershipFunction = deepcopy(fields[field][mf_name])
+            else:
+                mf: MembershipFunction = deepcopy(default_mfs[field])
+            expression.pop(0)
+
+            while len(expression) > 0:
+                if expression[0] not in hedges and expression[0] not in hedges_synonyms:
+                    raise SyntaxException(f"Unknown hedge {expression[0]} in expression {original_expression}")
+
+                if expression[0] in hedges.keys():
+                    mf.set_hedge(hedges[expression[0]](mf.hedge))
+                else:
+                    mf.set_hedge(hedges_synonyms[expression[0]][0](mf.hedge))
+                expression.pop(0)
+
+            l, r = mf.extract_range(alpha_cut)
+            crisp_query += f" {l} <= {field} and {field} <= r {token} "
+        else:
+            expression.append(token)
+
+    return crisp_query
