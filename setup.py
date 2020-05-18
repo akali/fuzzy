c4024c8d146942f25e715594380d269f32592076
akali <aisultan.kali@gmail.com>
2020-05-19 03:03:27 +0600

    minor release

--- setup.py
+++ setup.py
@@ -3,13 +3,16 @@ from distutils.core import setup
 setup(
     name='CWiPy',
     packages=['CWiPy'],
-    version='0.5.2',
+    version='0.5.3',
+
+    include_package_data=True,
+
     license='MIT',
     description='Computing with words library',
     author='Aisultan Kali',
     author_email='aisultan.kali@gmail.com',
     url='https://github.com/akali/fuzzy',
-    download_url='https://github.com/akali/fuzzy/archive/0.5.2.tar.gz',
+    download_url='https://github.com/akali/fuzzy/archive/0.5.3.tar.gz',
     keywords=['fuzzy', 'quantifier', 'very', 'expressive', 'CWiPy', 'modifier', 'hedge', 'query', 'database'],
     install_requires=[
         "pandas==1.0.3",
@@ -22,5 +25,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.8',
     ],
-    include_package_data=True
+    scripts=[
+        "scripts",
+    ]
 )
